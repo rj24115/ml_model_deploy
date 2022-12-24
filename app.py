a8a4a7a5d5d1d604a30ecec3eaef16ef3d2605b8
rj24115 <roman.jarocki@gmail.com>
2022-12-24 15:04:59 +0000

    adding json request, order may be wrong

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # RJ 23dec2023
 import numpy as np
 from flask import Flask, request, jsonify, render_template
+import json
 import pickle
 
 # Create flask app
@@ -8,7 +9,7 @@ flask_app = Flask(__name__)
 model = pickle.load(open("model.pkl", "rb")) #RF
 model = pickle.load(open("model2.pkl", "rb")) #LR
 
-
+# routing for displaying webpage
 @flask_app.route("/")
 def Home():
     return render_template("index.html")
@@ -23,23 +24,71 @@ def predict():
     prediction_text = f"The flower species is {prediction}, with probability {prediction_proba}"
     return render_template("index.html", prediction_text=prediction_text)
 
-# API enpoints
+# API enpoints - IRIS
 
+iris_dict = [
+    {'Sepal_Length':5,
+    'Sepal_Width':4,
+    'Petal_Length':1,
+    'Petal_Width':0.5 }
+]
+
+@flask_app.route('/iris')
+def get_iris():
+    return jsonify(iris_dict)
+
+@flask_app.route('/iris', methods=['POST'])
+def add_iris():
+    iris_dict.append(request.get_json())
+    return '', 204
+
+@flask_app.route('/iris_predict', methods=['POST'])
+def predict1():
+    received = request.get_json()
+    print(received.values())
+    # float_features = [float(x) for x in received]
+    # features = [np.array(float_features)]
+    features = np.array(list(received.values())).reshape(1, -1)
+    prediction = model.predict(features)
+    prediction_proba = model.predict_proba(features)
+
+    prediction_text = "some text"
+    prediction_text = f"The flower species is {prediction}, with probability {prediction_proba}"
+    
+    return prediction_text
+
+@flask_app.route('/iris_predict2', methods=['POST'])
+def predict2():
+    received = request.get_json()
+    sepal = request.args["Sepal_Length"]
+    print(sepal)
+    print(received.values())
+    # float_features = [float(x) for x in received]
+    # features = [np.array(float_features)]
+    # features = np.array(list(received.values())).reshape(1, -1)
+    # prediction = model.predict(features)
+    # prediction_proba = model.predict_proba(features)
+
+    prediction_text = "some text"
+    # prediction_text = f"The flower species is {prediction}, with probability {prediction_proba}"
+    
+    return prediction_text
+
+
+
+# API enpoints for income value
 incomes = [
     { 'description': 'salary', 'amount': 5000 }
 ]
-
 
 @flask_app.route('/incomes')
 def get_incomes():
     return jsonify(incomes)
 
-
 @flask_app.route('/incomes', methods=['POST'])
 def add_income():
     incomes.append(request.get_json())
     return 'added income', 204
-
 
 
  # **********
